0af7335976e019e28660afe26cffc586346f7f24
AsenR <rad1asen@proton.me>
2024-05-20 21:02:26 +0200

    Add files via upload
    fun.de

---
+++ FUNDAMENTALS/Basic Syntax, Conditional Statements and Loops - Lab/1_Number Definer.py
@@ -0,0 +1,19 @@
+Number=int(input())
+
+if Number > 0:
+    if Number < 1:
+        print("small positive")
+    elif Number > 1000000:
+        print("large positive")
+    else:
+        print("positive")
+    pass
+elif Number < 0:
+    
+    pass
+else:
+
+
+
+
+
